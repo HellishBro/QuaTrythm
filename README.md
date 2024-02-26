64a89bf55ee69c8462f086e85dd8bb73af152217
HellishBro <hellishbroamongus@gmail.com>
2024-02-26 07:48:04 -0800

    first commit from school

---
+++ README.md
@@ -0,0 +1,8 @@
+# QuaTrythm
+A 12-lane rhythm game
+
+---
+# Todo
+- [ ] Options / config menu
+- [ ] Making a tutorial
+- [ ] Making fomisses more readable
---
+++ src/options.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+from base_scene import Scene
+
+from utils import render_text, path, gradient, Timer
+from config import Config
+
+class Options(Scene):
+    def __init__(self):
+        pass
